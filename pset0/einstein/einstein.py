a828e4b286d792fb7a5cecb87f70b529875cc93b
Sneha Dubey <snehadubey517@gmail.com>
2025-11-02 22:42:54 +0530

    Refactor comments

--- pset0/einstein/einstein.py
+++ pset0/einstein/einstein.py
@@ -7,27 +7,20 @@
 # Description: This program prompts the user to enter a mass (in kilograms)
 #              and calculates the equivalent energy (in Joules) using
 #              Einstein's formula:
-#
 #                             E = m * c^2
-#
 #              where:
 #              E = energy in Joules
 #              m = mass in kilograms (user input)
 #              c = speed of light, approximately 300,000,000 m/s
-#
-#              The program assumes the user inputs an integer value for mass
-#              and outputs the resulting energy as an integer.
+#             The program assumes the user inputs an integer value for mass
+#             and outputs the resulting energy as an integer.
 #---------------------------------------------------------------------------------------------
 
 # Prompt the input from user for mass (in Kg)
 mass = int(input("m : "))
-
-# Define constant for speed of light (in meter per second)
-c = 300000000
-
+c = 300000000 # Define constant for speed of light (in meter per second)
 # Calculate energy using the formula E = m * c^2
 energy = mass * (c**2)
-
 # Print the results
 print("E :",energy)
 
